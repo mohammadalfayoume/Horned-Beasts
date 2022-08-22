e58bd8a96b7ec798b7a0d247c0fcc3ad9d2699d9
Mohammad Alfayoume <mabed3819@gmail.com>
2022-08-22 10:10:47 +0300

    Done

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -6,7 +6,7 @@ class HornedBeast extends React.Component {
             <div>
                 <div>
                     <h2>{this.props.title}</h2>
-                    <img src="https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=" alt="drink">{this.props.imageUrl}</img>
+                    <img src={this.props.imageUrl} alt="drink"></img>
                     <p>{this.props.description}</p>
                 </div>
             </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,8 +5,8 @@ class Main extends React.Component {
     render() {
         return(
             <div>
-                <HornedBeast title='This is title 1' description='This is description about first photo' />
-                <HornedBeast title='This is title 2' description='This is description about second photo' />
+                <HornedBeast title='This is title 1' imageUrl='https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=' description='This is description about first photo' />
+                <HornedBeast title='This is title 2' imageUrl='https://media.istockphoto.com/photos/business-man-pushing-large-stone-up-to-hill-business-heavy-tasks-and-picture-id825383494?k=20&m=825383494&s=612x612&w=0&h=tEqZ5HFZcM3lmDm_cmI7hOeceiqy9gYrkyLTTkrXdY4=' description='This is description about second photo' />
             </div>
         )
     }
